229843fcb8a9547ef9af9223bf0c9bb6851bb833
Blake R. Johnson <bjohnson@bbn.com>
2013-08-01 17:22:05 -0400

    Correct for direction of reduction operator in evolution_unitary.
    Also, add unitary_trajectory function to see the time evolution of an X and Z
    input state.

--- JuSimulator.jl
+++ JuSimulator.jl
@@ -17,10 +17,30 @@ function evolution_unitary(controlI, controlQ, calScale)
     const sy = 0.5*Complex128[0 -1im; 1im 0]
 
     Uprop = @parallel (*) for timect = 1:length(controlI)
-        expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, -1im*2*pi*timeStep*calScale)
+        expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, 1im*2*pi*timeStep*calScale)
     end
 
-    return Uprop
+    return Uprop'
+end
+
+function unitary_trajectory(controlI, controlQ, calScale)
+    const timeStep = 1.0
+    const sx = 0.5*Complex128[0 1; 1 0]
+    const sy = 0.5*Complex128[0 -1im; 1im 0]
+    const inputZ = Complex128[1, 0]
+    const inputX = Complex128[1, 1]/sqrt(2)
+
+    Uprop = eye(Complex128, 2)
+    finalZ = Array(Vector{Complex128}, length(controlI))
+    finalX = Array(Vector{Complex128}, length(controlI))
+
+    for timect = 1:length(controlI)
+        Uprop *= expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, 1im*2*pi*timeStep*calScale)
+        finalZ[timect] = Uprop' * inputZ
+        finalX[timect] = Uprop' * inputX
+    end
+
+    return finalZ, finalX
 end
 
 function run_sim(awgdata, chI, chQ, calScale)
