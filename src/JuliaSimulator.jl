fbb98063206b26b633d4c7da78c0c75cb014a0b7
Marcus Silva <msilva@bbn.com>
2013-11-07 17:02:25 -0500

    Fixed module names and export

--- src/JuliaSimulator.jl
+++ src/JuliaSimulator.jl
@@ -1,5 +1,9 @@
 module JuliaSimulator
 
+export evolution_unitary,
+       parallel_evolution_unitary,
+       unitary_trajectory
+
 using NumericExtensions
 
 function expm_eigen(A::Matrix, t)
@@ -55,7 +59,56 @@ function parallel_evolution_unitary(Hnat::Matrix{Complex128},
     return Uprop'
 end
 
-module JuliaSimulator.Test
+function evolution_unitary(controlI, controlQ, calScale)
+
+    const timeStep = 1.0
+    Hnat = eye(Complex128, 2)
+    tmpH = similar(Hnat)
+    const sx = 0.5*Complex128[0 1; 1 0]
+    const sy = 0.5*Complex128[0 -1im; 1im 0]
+
+    Uprop = @parallel (*) for timect = 1:length(controlI)
+        expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, 1im*2*pi*timeStep*calScale)
+    end
+
+    return Uprop'
+end
+
+function unitary_trajectory(controlI, controlQ, calScale)
+    const timeStep = 1.0
+    const sx = 0.5*Complex128[0 1; 1 0]
+    const sy = 0.5*Complex128[0 -1im; 1im 0]
+    const inputZ = Complex128[1, 0]
+    const inputX = Complex128[1, 1]/sqrt(2)
+
+    Uprop = eye(Complex128, 2)
+    finalZ = Array(Vector{Complex128}, length(controlI))
+    finalX = Array(Vector{Complex128}, length(controlI))
+
+    for timect = 1:length(controlI)
+        Uprop *= expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, 1im*2*pi*timeStep*calScale)
+        finalZ[timect] = Uprop' * inputZ
+        finalX[timect] = Uprop' * inputX
+    end
+
+    return finalZ, finalX
+end
+
+module Test
+
+export run_sim, sim_setup, setup_test, run_parallel_sim
+
+function run_sim(awgdata, chI, chQ, calScale)
+    results = Float64[]
+    initialRho = [1. 0.; 0. 0.]
+    const measOp = [1. 0.; 0. -1.]
+    for ct = 1:length(awgdata[chI])
+        U = evolution_unitary(awgdata[chI][ct], awgdata[chQ][ct], calScale)
+        finalRho = U * initialRho * U'
+        push!(results, real(trace(finalRho * measOp)))
+    end
+    return results
+end
 
 function sim_setup(dimension, 
                    numTimeSteps, 
