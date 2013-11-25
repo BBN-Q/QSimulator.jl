88ed1eb95eb5fc690b4011b1fab748cad20de13c
Marcus P S <marcus.ps+github@gmail.com>
2013-11-24 20:54:07 -0500

    Added first pass at expansion of dissipators

--- src/dissipation.jl
+++ src/dissipation.jl
@@ -1,5 +1,11 @@
 #using QIP
 
+export Dissipation,
+       label,
+       rate,
+       Cooling,
+       generator
+
 abstract Dissipation
 
 label(d::Dissipation) = d.label
@@ -12,21 +18,30 @@ type Cooling <: Dissipation
 end
 
 function generator(d::Cooling, t::Real)
-  local a
+  local a, n, i
   a = lowering(d.sys)
-  return rate(d)*QIP.dissipator(a)
+  n = a'*a
+  # return pairs of left/right linear operators
+  return [(sqrt(rate)*a,sqrt(rate)*a'),
+          (-1/2*rate(d)*n,id),
+          (id,-1/2*rate(d)*n)]
 end
 
-function generator(sys::CompositeQSystem, t::Real)
-  local H, Gsop
+function generator(sys::CompositeQSystem, t::Real=0.)
+  local H, Gsop, id, d
   H = hamiltonian(sys, t)
-  # [todo: blargh]
-  Gsop = QIP.hamiltonian(H)
+  d = size(H,1)
+  id = eye(d)
 
-  for (ct, d) in enumerate(sys.dissipators)
-    Gsop += generator(d)
+  Gsop = QIP.liou(-1im*H,id)
+  add!(Gsop, QIP.liou(id,1im*H))
+
+  for (ct, diss) in enumerate(sys.dissipators)
+      for sop in generator(diss,t)
+          add!(Gsop, 
+               QIP.liou(expand(sop[1],c.subSystemExpansions[ct],d),
+                        expand(sop[2],c.subSystemExpansions[ct],d)))
+      end
   end
-end
-
-function 
+  return Gsop
 end
