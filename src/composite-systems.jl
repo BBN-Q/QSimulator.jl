f5feb53cd3bc0412b1c3f017a14ac5c32f74c25d
Colm Ryan <colm@colmryan.org>
2013-12-03 10:28:50 -0500

    Clean out old evolution testing methods and bring unitary_propagator into evolution.jl

--- src/composite-systems.jl
+++ src/composite-systems.jl
@@ -146,26 +146,3 @@ function expand_indices(actingOn::Vector, dims::Vector)
     M = expand(reshape(1:lenm, sm), actingOn, dims)
     return [find(M .== x) for x in 1:lenm]
 end
-
-
-function expm_eigen(A::Matrix, t)
-    #Calculates expm(t*A) via eigenvalue decomposition and assuming Hermitian matrix
-    F = eigfact(Hermitian(A))
-
-    # V * diagm(exp(t*D)) * V'
-    return scale(F[:vectors], exp(t*F[:values])) * F[:vectors]'
-end
-
-function unitary_propagator(sys::CompositeQSystem, timeStep::Float64, startTime::Float64, endTime::Float64)
-
-    #Preallocate Hamiltonian memory
-    Ham = zeros(Complex128, (dim(sys), dim(sys)))
-    Uprop = @parallel (*) for time = startTime:timeStep:endTime
-        #a *= b expands to a = a*b
-        hamiltonian_add!(Ham, sys, time)
-        expm_eigen(Ham, 1im*timeStep)
-    end
-    return Uprop'
-end
-
-
--- src/evolution.jl
+++ src/evolution.jl
@@ -1,100 +1,25 @@
 export ## Methods
-       evolution_unitary,
-       parallel_evolution_unitary,
-       run_sim,
-       unitary_trajetory
+       unitary_propagator
 
-function evolution_unitary(Hnat::Matrix{Complex128}, 
-                           controlHams, 
-                           controlFields::Matrix{Float64}, 
-                           controlFreqs::Vector{Float64})
 
-    const timeStep = 0.01
-    Uprop = eye(Complex128, size(Hnat,1))
-    tmpH = similar(Hnat)
-    localH = similar(Hnat)
+function expm_eigen(A::Matrix, t)
+    #Calculates expm(t*A) via eigenvalue decomposition and assuming Hermitian matrix
+    F = eigfact(Hermitian(A))
 
-    for timect = 1:size(controlFields,2)
-        tmpH[:] = Hnat
-        for controlct = 1:size(controlFields,1)
-            localH[:] = controlHams[controlct]
-            multiply!(localH, controlFields[controlct, timect]*cos(2*pi*timeStep*timect*controlFreqs[controlct]))
-            add!(tmpH, localH)
-        end
-        Uprop *= expm_eigen(tmpH, 1im*2*pi*timeStep)
+    # V * diagm(exp(t*D)) * V'
+    return scale(F[:vectors], exp(t*F[:values])) * F[:vectors]'
+end
+
+function unitary_propagator(sys::CompositeQSystem, timeStep::Float64, startTime::Float64, endTime::Float64)
+
+    #Preallocate Hamiltonian memory
+    Ham = zeros(Complex128, (dim(sys), dim(sys)))
+    Uprop = @parallel (*) for time = startTime:timeStep:endTime
+        #a *= b expands to a = a*b
+        hamiltonian_add!(Ham, sys, time)
+        expm_eigen(Ham, 1im*timeStep)
     end
-
     return Uprop'
 end
 
 
-function parallel_evolution_unitary(Hnat::Matrix{Complex128}, 
-                                    controlHams, 
-                                    controlFields::Matrix{Float64}, 
-                                    controlFreqs::Vector{Float64})
-
-    const timeStep = 0.01
-    tmpH = similar(Hnat)
-    localH = similar(Hnat)
-
-    Uprop = @parallel (*) for timect = 1:size(controlFields,2)
-        tmpH[:] = Hnat
-        for controlct = 1:size(controlFields,1)
-            localH[:] = controlHams[controlct]
-            multiply!(localH, controlFields[controlct, timect]*cos(2*pi*timeStep*timect*controlFreqs[controlct]))
-            add!(tmpH, localH)
-        end
-        expm_eigen(tmpH, 1im*2*pi*timeStep)
-    end
-
-    return Uprop'
-end
-
-# function unitary_propagator(sys, timeStep)
-#     #Assume we start at 0.0
-
-
-function evolution_unitary(controlI, controlQ, calScale)
-
-    const timeStep = 1.0
-    const sx = 0.5*Complex128[0 1; 1 0]
-    const sy = 0.5*Complex128[0 -1im; 1im 0]
-
-    Uprop = @parallel (*) for timect = 1:length(controlI)
-        expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, 1im*2*pi*timeStep*calScale)
-    end
-
-    return Uprop'
-end
-
-function unitary_trajectory(controlI, controlQ, calScale)
-    const timeStep = 1.0
-    const sx = 0.5*Complex128[0 1; 1 0]
-    const sy = 0.5*Complex128[0 -1im; 1im 0]
-    const inputZ = Complex128[1, 0]
-    const inputX = Complex128[1, 1]/sqrt(2)
-
-    Uprop = eye(Complex128, 2)
-    finalZ = Array(Vector{Complex128}, length(controlI))
-    finalX = Array(Vector{Complex128}, length(controlI))
-
-    for timect = 1:length(controlI)
-        Uprop *= expm_eigen(controlI[timect]*sx + controlQ[timect]*sy, 1im*2*pi*timeStep*calScale)
-        finalZ[timect] = Uprop' * inputZ
-        finalX[timect] = Uprop' * inputX
-    end
-
-    return finalZ, finalX
-end
-
-function run_sim(awgdata, chI, chQ, calScale)
-    results = Float64[]
-    initialRho = [1. 0.; 0. 0.]
-    const measOp = [1. 0.; 0. -1.]
-    for ct = 1:length(awgdata[chI])
-        U = evolution_unitary(awgdata[chI][ct], awgdata[chQ][ct], calScale)
-        finalRho = U * initialRho * U'
-        push!(results, real(trace(finalRho * measOp)))
-    end
-    return results
-end
