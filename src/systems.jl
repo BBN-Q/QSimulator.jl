0c1ae383d1c199e6a7eda8107143bc629318d451
Marcus P S <msilva@bbn.com>
2014-01-17 13:57:05 -0500

    Made TunableTransmon abstract

--- src/systems.jl
+++ src/systems.jl
@@ -1,6 +1,7 @@
 export Transmon,
        Resonator,
        TunableDuffingTransmon,
+       TunableFullTransmon,
        TunableTransmon,
        Qubit,
        Duffing
@@ -12,8 +13,6 @@ type Resonator <: QSystem
     dim::Int
 end 
 hamiltonian(r::Resonator) = r.freq*number(r)
-
-#Duffing approximation to transmons
 
 #Fixed frequency transmon 
 type Transmon <: QSystem
@@ -32,7 +31,10 @@ end
 hamiltonian(t::Transmon) = sqrt(8*t.E_J*t.E_C)*number(t) - t.E_C/12*(X(t)^4)
 
 #Tunable transmon 
-type TunableTransmon <: QSystem
+
+abstract TunableTransmon <: QSystem
+
+type TunableFullTransmon <: TunableTransmon
     label::String
     E_C::Float64
     E_J::Float64 #sum of junction E_J's
@@ -41,18 +43,19 @@ type TunableTransmon <: QSystem
     fluxBias::Float64 # flux bias in units of Phi_0
     flux::Float64 #total flux in units of Phi_0
 end
-TunableTransmon(label::String, E_C::Float64, E_J::Float64, d::Float64, dim::Int, fluxBias::Float64) = TunableTransmon(label, E_C, E_J, d, dim, fluxBias, fluxBias)
+TunableFullTransmon(label::String, E_C::Float64, E_J::Float64, d::Float64, dim::Int, fluxBias::Float64) = 
+  TunableFullTransmon(label, E_C, E_J, d, dim, fluxBias, fluxBias)
 
 #Helper function to calculate effective EJ for a transmon
 scale_EJ(flux::Float64, d::Float64) = cos(pi*flux)*sqrt(1 + d^2*(tan(pi*flux)^2))
 
-function hamiltonian(tt::TunableTransmon, t::Float64=0.0)
+function hamiltonian(tt::TunableFullTransmon, t::Float64=0.0)
     myE_J = tt.E_J*scale_EJ(tt.flux, tt.d)
     return (sqrt(8*tt.E_C*myE_J)*number(tt) - (1.0/12)*tt.E_C*(X(tt)^4))
 end
 
-#Tunable transmon 
-type TunableDuffingTransmon <: QSystem
+#Tunable Duffing approx. transmon 
+type TunableDuffingTransmon <: TunableTransmon
     label::String
     E_C::Float64
     E_J::Float64 #sum of junction E_J's
