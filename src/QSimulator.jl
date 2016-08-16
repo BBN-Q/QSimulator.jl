e46bca739bcc812e5ff956c0e4bd85c343fc2616
Blake R. Johnson <bjohnson@bbn.com>
2016-08-16 16:46:30 -0400

    Remove paulis.jl
    None of the methods were used by the rest of the package. Cliffords.jl provides
    a more complete Pauli type.

--- src/QSimulator.jl
+++ src/QSimulator.jl
@@ -8,21 +8,12 @@ using Grid,
 import Base: getindex, +
 
 include("base-types.jl")
-
 include("systems.jl")
-
 include("controls.jl")
-
 include("interactions.jl")
-
 include("composite-systems.jl")
-
 include("dissipation.jl")
-
 include("evolution.jl")
-
 include("read_APS_file.jl")
-
-include("paulis.jl")
 
 end
