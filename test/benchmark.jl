c82e8695d76cb5cd44bb351b6da6b2c059eb2bb4
Colm Ryan <colm@rigetti.com>
2018-02-13 21:54:37 -0800

    add rabi flops to benchmark

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -39,8 +39,33 @@ for n = 2:4
     state_suite["dipole chain ($n transmons)"] = @benchmarkable unitary_state($cqs, $times, $ψ0);
 end
 
-
 # Lab frame Rabi oscillations of an n level transmon
+prop_suite = suite["unitary"]["propagator"]["rabi flops"] = BenchmarkGroup()
+state_suite = suite["unitary"]["pure state"]["rabi flops"] = BenchmarkGroup()
+
+# TODO replace when available in QSimulator
+function qubit_drive(q::QSimulator.QSystem, drive::Function)
+    function add_drive_ham!(ham, idxs, t)
+        pulse = 2π * drive(t)
+        drive_ham = real(pulse) * X(q) + imag(pulse) * Y(q)
+        QSimulator.expand_add!(ham, drive_ham, idxs)
+    end
+end
+
+qubit_freq = 5.0
+nutation_freq = 0.02
+for n = 2:4
+    q0 = FixedDuffingTransmon("q0", qubit_freq, -0.2, n)
+    cqs = CompositeQSystem([q0]);
+    add_hamiltonian!(cqs, hamiltonian(q0), "q0")
+    add_hamiltonian!(cqs, qubit_drive(q0, t -> nutation_freq*cos(2π*qubit_freq * t)), q0);
+    ψ0 = zeros(Complex128, n)
+    ψ0[1] = 1
+    times = collect(linspace(0,100,101))
+    prop_suite["$n level transmon"] = @benchmarkable unitary_propagator($cqs, $times);
+    state_suite["$n level transmon"] = @benchmarkable unitary_state($cqs, $times, $ψ0);
+end
+
 
 # Lab frame parmetric interaction between two transmons with spectators
 
