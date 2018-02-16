2bfc41c49d799fed479623caf985ab4ba14ab337
Colm Ryan <colm@rigetti.com>
2018-02-15 22:12:40 -0800

    :racehorse: continue with work hamiltonian for state evolutions
    julia> showall(judge(median(results), median(master_results)))
    1-element BenchmarkTools.BenchmarkGroup:
      tags: []
      "unitary" => 2-element BenchmarkTools.BenchmarkGroup:
    	  tags: []
    	  "propagator" => 3-element BenchmarkTools.BenchmarkGroup:
    		  tags: []
    		  "free evolution" => 6-element BenchmarkTools.BenchmarkGroup:
    			  tags: []
    			  "single transmon (2 levels)" => TrialJudgement(+5.04% => regression)
    			  "dipole chain (3 transmons)" => TrialJudgement(+1.86% => invariant)
    			  "dipole chain (2 transmons)" => TrialJudgement(+2.15% => invariant)
    			  "single transmon (4 levels)" => TrialJudgement(+8.25% => regression)
    			  "dipole chain (4 transmons)" => TrialJudgement(+0.80% => invariant)
    			  "single transmon (3 levels)" => TrialJudgement(+0.03% => invariant)
    		  "rabi flops" => 3-element BenchmarkTools.BenchmarkGroup:
    			  tags: []
    			  "2 level transmon" => TrialJudgement(+11.44% => regression)
    			  "3 level transmon" => TrialJudgement(+18.78% => regression)
    			  "4 level transmon" => TrialJudgement(+6.01% => regression)
    		  "parametric 2Q gate" => 3-element BenchmarkTools.BenchmarkGroup:
    			  tags: []
    			  "3 transmons" => TrialJudgement(-3.32% => invariant)
    			  "4 transmons" => TrialJudgement(-1.76% => invariant)
    			  "2 transmons" => TrialJudgement(-4.43% => invariant)
    	  "pure state" => 3-element BenchmarkTools.BenchmarkGroup:
    		  tags: []
    		  "free evolution" => 6-element BenchmarkTools.BenchmarkGroup:
    			  tags: []
    			  "single transmon (2 levels)" => TrialJudgement(-9.63% => improvement)
    			  "dipole chain (3 transmons)" => TrialJudgement(-68.67% => improvement)
    			  "dipole chain (2 transmons)" => TrialJudgement(-33.44% => improvement)
    			  "single transmon (4 levels)" => TrialJudgement(-26.27% => improvement)
    			  "dipole chain (4 transmons)" => TrialJudgement(-62.98% => improvement)
    			  "single transmon (3 levels)" => TrialJudgement(-15.32% => improvement)
    		  "rabi flops" => 3-element BenchmarkTools.BenchmarkGroup:
    			  tags: []
    			  "2 level transmon" => TrialJudgement(-3.09% => invariant)
    			  "3 level transmon" => TrialJudgement(-9.60% => improvement)
    			  "4 level transmon" => TrialJudgement(-11.47% => improvement)
    		  "parametric 2Q gate" => 3-element BenchmarkTools.BenchmarkGroup:
    			  tags: []
    			  "3 transmons" => TrialJudgement(-64.34% => improvement)
    			  "4 transmons" => TrialJudgement(-57.73% => improvement)
    			  "2 transmons" => TrialJudgement(-32.95% => improvement)
    
    julia> difference = judge(median(results), median(master_results))
    1-element BenchmarkTools.BenchmarkGroup:
      tags: []
      "unitary" => 2-element BenchmarkGroup([])
    
    julia> difference["unitary"]["pure state"]["free evolution"]["dipole chain (4 transmons)"]
    BenchmarkTools.TrialJudgement:
      time:   -62.98% => improvement (5.00% tolerance)
      memory: -99.69% => improvement (1.00% tolerance)
    
    julia> difference["unitary"]["pure state"]["parametric 2Q gate"]["3 transmons"]
    BenchmarkTools.TrialJudgement:
      time:   -64.34% => improvement (5.00% tolerance)
      memory: -97.92% => improvement (1.00% tolerance)

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -45,13 +45,16 @@ function unitary_state(cqs::CompositeQSystem, ts::Vector, ψ0::Vector; t0=0.0)
     # schrodinger differential equation for state vector with in place update
     # dψ/dt = -iHψ
     function ode(dψ, ψ, p, t)
-        ham = copy(p[1])
-        add_parametric_hamiltonians!(ham, p[2], t)
-        dψ[:] = vec(-1im * ham * ψ)
+        ham = p[3] # preallocated workspace array
+        ham .= p[2] # start from fixed_ham
+        add_parametric_hamiltonians!(ham, p[1], t)
+        scale!(ham, -1im)
+        A_mul_B!(dψ, ham, ψ)
     end
     # scale Hamiltonian from Hz to rad.
     fixed_ham = 2pi * hamiltonian(cqs)
-    prob = ODEProblem(ode, ψ0, (t0, float(ts[end])), (fixed_ham, cqs))
+    work_ham = similar(fixed_ham)
+    prob = ODEProblem(ode, ψ0, (t0, float(ts[end])), (cqs, fixed_ham, work_ham))
     save_start = ts[1]==t0 ? true : false #save t0 only if asked for
     sol = solve(prob; saveat=ts, save_start=save_start)
     sol.u
@@ -67,13 +70,15 @@ function unitary_state(cqs::CompositeQSystem, ts::Vector, ρ0::Matrix; t0=0.0)
     # schrodinger differential equation for density matrix with in place update
     # dρ/dt = -i[H, ρ]
     function ode(dρ, ρ, p, t)
-        ham = copy(p[1])
-        add_parametric_hamiltonians!(ham, p[2], t)
-        dρ[:] = vec(-1im * (ham*ρ - ρ*ham))
+        ham = p[3] # preallocated workspace array
+        ham .= p[2] # start from fixed_ham
+        add_parametric_hamiltonians!(ham, p[1], t)
+        dρ .= -1im * (ham*ρ - ρ*ham)
     end
     # scale Hamiltonian from Hz to rad.
     fixed_ham = 2pi * hamiltonian(cqs)
-    prob = ODEProblem(ode, ρ0, (t0, float(ts[end])), (fixed_ham, cqs))
+    work_ham = similar(fixed_ham)
+    prob = ODEProblem(ode, ρ0, (t0, float(ts[end])), (cqs, fixed_ham, work_ham))
     save_start = ts[1]==t0 ? true : false #save t0 only if asked for
     sol = solve(prob; saveat=ts, save_start=save_start)
     sol.u
