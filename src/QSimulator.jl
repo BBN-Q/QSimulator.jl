bb26b0e44e0bb6e23c51ae7f1f1f5c6ed1d7ecf3
Colm Ryan <colm@rigetti.com>
2018-03-15 10:57:25 -0700

    tweak ordering of includes to match dependencies

--- src/QSimulator.jl
+++ src/QSimulator.jl
@@ -6,7 +6,7 @@ const ⊗ = kron
 
 include("systems.jl")
 include("composite_systems.jl")
+include("hamiltonians.jl")
 include("parametric_hamiltonians.jl")
 include("time_evolution.jl")
-include("hamiltonians.jl")
 end
