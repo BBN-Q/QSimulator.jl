cec2a9152b65b4ed34a1d5aee7c28869f09143bf
Colm Ryan <colm@rigetti.com>
2018-04-19 21:43:13 -0700

    :art: put Nicometrics into module so we don't load so much into QSimulator namespace

--- src/QSimulator.jl
+++ src/QSimulator.jl
@@ -7,6 +7,8 @@ const ⊗ = kron
 include("systems.jl")
 include("composite_systems.jl")
 include("nicometrics.jl")
+using .Nicometrics
+
 include("hamiltonians.jl")
 include("parametric_hamiltonians.jl")
 include("time_evolution.jl")
