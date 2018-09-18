a18a307fe9d9a1865a9f59146d9d9c3e560909f7
alexander papageorge <alexander@rigetti.com>
2018-09-18 12:52:58 -0700

    removed double and non-identical flux_drive and dipole_drive

--- src/operators.jl
+++ src/operators.jl
@@ -1,5 +1,5 @@
 export raising, lowering, number, X, Y, X_Y, rotating_operator,
-       decay, dephasing, dipole_drive, flux_drive
+       decay, dephasing
 
 ######################################################
 # Primitives
@@ -49,56 +49,12 @@ The lindblad operator for decay.
 """
 dephasing(qs::QSystem, γ::Real) = sqrt(2γ) * number(qs)
 
-"""
-    dipole_drive(qs::QSystem, drive::Function)
-
-Given some function of time, return a function applying a time dependent
-dipole Hamiltonian.
-
-## args
-* `qs`: a QSystem.
-* `drive`: a function of time returning a real or complex value. The real
-    part couples to X and the imaginary part couples to Y.
-
-## returns
-A function of time.
-"""
-function dipole_drive(qs::QSystem, drive::Function)
-    x_ham = X(qs)
-    y_ham = Y(qs)
-    function ham(t)
-        pulse = drive(t)
-        return real(pulse) * x_ham + imag(pulse) * y_ham
-    end
-    return ham
-end
-
-
-"""
-    flux_drive(qs::QSystem, drive::Function)
-
-Given some function of time, return a function applying a
-time dependent Hamiltonian.
-
-## args
-* `qs`: a QSystem with a method of `hamiltonian` accepting a function of time.
-* `drive`: a function of time returning a real value.
-
-## returns
-A function of time.
-"""
-flux_drive(qs::QSystem, drive::Function) = hamiltonian(qs, drive(t))
 
 ######################################################
 # Backwards compatibility
 ######################################################
 
-export microwave_drive, dipole, flip_flop, XY, rotating_flip_flop
-
-function microwave_drive(q::QSystem, drive::Function)
-    warn("Deprecation warning: microwave_drive.")
-    return dipole_drive(q, drive)
-end
+export dipole, flip_flop, XY, rotating_flip_flop
 
 function dipole(a::QSystem, b::QSystem)
     warn("Deprecation warning: dipole.")
