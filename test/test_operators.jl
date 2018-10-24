fd893fb02360c75ff80de9d65cc45af8c4c682cc
Michael Scheer <michael@rigetti.com>
2018-10-23 22:03:17 -0700

    make a testset for the resonator test

--- test/test_operators.jl
+++ test/test_operators.jl
@@ -1,5 +1,7 @@
 using Test, QSimulator
 
 # test harmonic oscillator operator identity a†a = N
-r = Resonator("test", 3, QSimulator.ResonatorSpec(5.5))
-@test isapprox(raising(r) * lowering(r), number(r))
+@testset "Resonator" begin
+    r = Resonator("test", 3, QSimulator.ResonatorSpec(5.5))
+    @test isapprox(raising(r) * lowering(r), number(r))
+end
