fe16029bf70c1e2897151c87818a1709815c1d26
Michael Scheer <michael@rigetti.com>
2018-10-25 13:15:23 -0700

    make choose_times_floquet not error when the width is 0 or dt is 0

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -236,13 +236,19 @@ Choose times for integration that maximize remainder overlap for Floquet integra
 An array of times.
 """
 function choose_times_floquet(center::Real, width::Real, t_period::Real, dt::Real)
-    dt = t_period / ceil(Int, t_period / dt) # reset dt so it divides time_period, making it smaller
-    num_times = floor(Int, width / dt)
-    num_times += iseven(num_times) # make sure num_times is odd for symmetry
-    width = dt * num_times
-    times = collect(range(center - width/2, stop=center + width/2, length=num_times))
-    times[ceil(Int, num_times/2)] = center
-    return times
+    @assert width >= 0
+    @assert dt >= 0
+    if (width == 0) || (dt == 0)
+        return [center]
+    else
+        dt = t_period / ceil(Int, t_period / dt) # reset dt so it divides time_period, making it smaller
+        num_times = floor(Int, width / dt)
+        num_times += iseven(num_times) # make sure num_times is odd for symmetry
+        width = dt * num_times
+        times = collect(range(center - width/2, stop=center + width/2, length=num_times))
+        times[ceil(Int, num_times/2)] = center
+        return times
+    end
 end
 
 """
