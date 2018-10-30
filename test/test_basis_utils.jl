a2139d5ed639ebdc97fd6a04bbbb42b882aa068e
Colm Ryan <colm@rigetti.com>
2018-10-29 17:03:24 -0700

    :green_check: update the unit tests for the basis state utilites refactor

---
+++ test/test_basis_utils.jl
@@ -0,0 +1,17 @@
+using Test, QSimulator
+
+@testset "basis state utilities" begin
+    # simple tensor product expansion
+    basis = QSimulator.TensorProductBasis((2,2))
+    @test [bs.states for bs in QSimulator.basis_states(basis)] == [(0,0), (0,1), (1,0), (1,1)]
+    basis = QSimulator.TensorProductBasis((2,3))
+    @test [bs.states for bs in QSimulator.basis_states(basis)] == [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2)]
+
+    # go from state -> index
+    basis = QSimulator.TensorProductBasis((2,3,4,5))
+    all_states = QSimulator.basis_states(basis)
+    @test all(QSimulator.index(state) == i for (i,state) in enumerate(all_states))
+
+    # and from index to state
+    @test all(basis[i] == state for (i,state) in enumerate(all_states))
+end
