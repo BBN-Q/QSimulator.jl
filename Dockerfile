695cc20e14ac6600c09b41a462e91583b6fd638f
Dan Girshovich <dan@rigetti.com>
2018-12-04 12:09:23 -0800

    rework dockerfile + scripts to use hosts package cache

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,21 @@ FROM library/julia:1.0.2
 
 RUN apt-get update && apt-get install -yq bzip2
 
-WORKDIR /qsimulator
+# The ~/.julia from the host is mounted here
+ENV JULIA_DEPOT_PATH /opt/.julia
+RUN mkdir -p /opt/.julia
 
-COPY Manifest.toml .
-COPY Project.toml .
-COPY scripts ./scripts
+# The project source is mounted here
+WORKDIR /project-mount
 
-RUN /qsimulator/scripts/build.sh
+# COPY Manifest.toml .
+# COPY Project.toml .
+# COPY scripts ./scripts
 
-COPY . .
+# RUN /qsimulator/scripts/build.sh
 
-CMD ["/qsimulator/scripts/runtests.sh"]
+# COPY . .
+
+# RUN julia /qsimulator/scripts/setup.jl /qsimulator
+
+CMD ["julia"]
--- scripts/run-docker-tests.sh
+++ scripts/run-docker-tests.sh
@@ -7,6 +7,17 @@ cd "$(dirname "$0")"
 # Switch to project base directory
 pushd ../
 
-docker build -f Dockerfile -t qsimulator-tests .
+# Create a Julia depot on the worker, if none exists
+# This acts as a global cache for Julia dependencies
+# The declarative nature of Pkg in Julia 1.0 should mean
+# that this is a safe optimization.
+mkdir -p ~/.julia
 
-docker run qsimulator-tests
+# Build base with system deps
+docker build -f Dockerfile -t julia-base .
+
+# Build deps. Cache on host
+docker run -v ~/.julia:/opt/.julia -v $(pwd):/project-mount julia-base julia --project setup.jl
+
+# Run tests
+docker run -v ~/.julia:/opt/.julia -v $(pwd):/project-mount julia-base /project-mount/scripts/runtests.sh
