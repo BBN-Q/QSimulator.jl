fc34a6691b92a8b89056b03483dca4783d52afd0
Dan Girshovich <dan@rigetti.com>
2018-12-07 10:15:34 -0800

    Update Dockerfile + ci scripts to use a Docker volume for the julia depot
    This allows the host to persist a global cache for the compiled dependencies.

--- ci/run-docker-tests.sh
+++ ci/run-docker-tests.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")"
 # Switch to project base directory
 pushd ../
 
-docker build -f Dockerfile -t qsimulator-tests .
+docker build -f Dockerfile -t example-tests .
 
-docker run qsimulator-tests
+docker run -v julia-depot:/root/.julia example-tests
