f11777338797613411ec31d9ac1caf34f6e3adbc
Dan Girshovich <dan@rigetti.com>
2018-12-07 10:22:58 -0800

    example-tests -> qsimulator_tests in run-docker-tests.sh

--- ci/run-docker-tests.sh
+++ ci/run-docker-tests.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")"
 # Switch to project base directory
 pushd ../
 
-docker build -f Dockerfile -t example-tests .
+docker build -f Dockerfile -t qsimulator-tests .
 
-docker run -v julia-depot:/root/.julia example-tests
+docker run -v julia-depot:/root/.julia qsimulator-tests
