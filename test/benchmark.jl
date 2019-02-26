4667986d1a4309d6c8b62c18559af0926c184ce3
Colm Ryan <colm@rigetti.com>
2019-02-26 13:30:05 -0800

    get benchmarking suite working again

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -12,31 +12,31 @@ suite["unitary"]["pure state"] = BenchmarkGroup()
 prop_suite = suite["unitary"]["propagator"]["free evolution"] = BenchmarkGroup()
 state_suite = suite["unitary"]["pure state"]["free evolution"] = BenchmarkGroup()
 for n = 2:4
-    q0 = FixedDuffingTransmon("q0", 5, -0.2, n)
+    q0 = DuffingTransmon("q0", n, DuffingSpec(5, -0.2))
     cqs = CompositeQSystem([q0])
     add_hamiltonian!(cqs, q0)
-    times = collect(range(0,stop=1.0,length=201))
+    times = range(0, stop=1.0, length=201)
     # superposition of all levels
-    ψ0 = (1/sqrt(dimension(cqs))) * ones(ComplexF64, dimension(cqs))
+    ψ₀ = (1/sqrt(dimension(cqs))) * ones(ComplexF64, dimension(cqs))
     prop_suite["single transmon ($n levels)"] = @benchmarkable unitary_propagator($cqs, $times);
-    state_suite["single transmon ($n levels)"] = @benchmarkable unitary_state($cqs, $times, $ψ0);
+    state_suite["single transmon ($n levels)"] = @benchmarkable unitary_state($cqs, $times, $ψ₀);
 end
 
 # Dipole interaction between a chain of fixed transmons
 for n = 2:4
-    qs = [FixedDuffingTransmon("q$ct", 4.0 + 0.1*ct, -(0.2 + 0.01*ct), 3) for ct = 0:(n-1)]
+    qs = [DuffingTransmon("q$ct", 3, DuffingSpec(4.0 + 0.1*ct, -(0.2 + 0.01*ct))) for ct = 0:(n-1)]
     cqs = CompositeQSystem(qs)
     for q = qs
         add_hamiltonian!(cqs, hamiltonian(q), q)
     end
     for (qa,qb) = zip(qs[1:end-1], qs[2:end])
-        add_hamiltonian!(cqs, 0.005*dipole(qa, qb), [qa, qb])
+        add_hamiltonian!(cqs, 0.005*X([qa, qb]), [qa, qb])
     end
-    times = collect(range(0,stop=1.0,length=201))
+    times = range(0, stop=1.0, length=201)
     # superposition of all levels
-    ψ0 = (1/sqrt(dimension(cqs))) * ones(ComplexF64, dimension(cqs))
+    ψ₀ = (1/sqrt(dimension(cqs))) * ones(ComplexF64, dimension(cqs))
     prop_suite["dipole chain ($n transmons)"] = @benchmarkable unitary_propagator($cqs, $times);
-    state_suite["dipole chain ($n transmons)"] = @benchmarkable unitary_state($cqs, $times, $ψ0);
+    state_suite["dipole chain ($n transmons)"] = @benchmarkable unitary_state($cqs, $times, $ψ₀);
 end
 
 # Lab frame Rabi oscillations of an n level transmon
@@ -46,15 +46,16 @@ state_suite = suite["unitary"]["pure state"]["rabi flops"] = BenchmarkGroup()
 qubit_freq = 5.0
 nutation_freq = 0.02
 for n = 2:4
-    q0 = FixedDuffingTransmon("q0", qubit_freq, -0.2, n)
+    q0 = DuffingTransmon("q0", n, DuffingSpec(qubit_freq, -0.2))
     cqs = CompositeQSystem([q0]);
     add_hamiltonian!(cqs, q0)
-    add_hamiltonian!(cqs, microwave_drive(q0, t -> nutation_freq*cos(2π*qubit_freq * t)), q0);
-    ψ0 = zeros(ComplexF64, n)
-    ψ0[1] = 1
-    times = collect(range(0,stop=100,length=101))
+    add_hamiltonian!(cqs, dipole_drive(q0, t -> nutation_freq*cos(2π*qubit_freq * t)), q0);
+    # start in ground state
+    ψ₀ = zeros(ComplexF64, n)
+    ψ₀[1] = 1
+    times = range(0, stop=100, length=101)
     prop_suite["$n level transmon"] = @benchmarkable unitary_propagator($cqs, $times);
-    state_suite["$n level transmon"] = @benchmarkable unitary_state($cqs, $times, $ψ0);
+    state_suite["$n level transmon"] = @benchmarkable unitary_state($cqs, $times, $ψ₀);
 end
 
 # Lab frame parametric interaction between two transmons with spectators
@@ -63,11 +64,11 @@ state_suite = suite["unitary"]["pure state"]["lab frame parametric 2Q gate"] = B
 
 # helper function to add flux drive
 for n = 2:3
-    q0 = FixedDuffingTransmon("q0", 3.94015, -0.1807,  3)
-    q1 = TunableDuffingTransmon("q1",  0.172, 16.4, 0.55, 3)
+    q0 = DuffingTransmon("q0", 3, DuffingSpec(3.94015, -0.1807))
+    q1 = PerturbativeTransmon("q1",  3, TransmonSpec(0.172, 16.4, 0.55))
 
     # add fixed frequency spectators
-    spectator_qs = [FixedDuffingTransmon("q$ct", 4.0 + 0.1*ct, -(0.2 + 0.01*ct), 3) for ct = 2:(n-1)]
+    spectator_qs = [DuffingTransmon("q$ct", 3, DuffingSpec(4.0 + 0.1*ct, -(0.2 + 0.01*ct))) for ct = 2:(n-1)]
 
     # should get an iSWAP interaction at ≈ 122 MHz with drive amplitude 0.323 Φ₀
     mod_freq = 122.1/1e3
@@ -75,23 +76,23 @@ for n = 2:3
     all_qs = [q0, q1, spectator_qs...]
     cqs = CompositeQSystem(all_qs)
     add_hamiltonian!(cqs, q0)
-    add_hamiltonian!(cqs, flux_drive(q1, t -> sin(2π*mod_freq*t)), q1)
-    add_hamiltonian!(cqs, 0.006*dipole(q0, q1), [q0,q1])
+    add_hamiltonian!(cqs, parametric_drive(q1, t -> sin(2π*mod_freq*t)), q1)
+    add_hamiltonian!(cqs, 0.006*X([q0, q1]), [q0, q1])
 
     # add hamiltoians for spectators coupled to tunable transmon
     for q = all_qs[3:end]
         add_hamiltonian!(cqs, q)
-        add_hamiltonian!(cqs, 0.006*dipole(q, q1), [q,q1])
+        add_hamiltonian!(cqs, 0.006*X([q, q1]), [q, q1])
     end
 
-    times = collect(0:200)
-    ψ0 = ComplexF64[0.0; 1.0; 0.0] ⊗ ComplexF64[1.0; 0.0; 0.0] # start in 10 state
+    times = 0:200
+    ψ₀ = ComplexF64[0.0; 1.0; 0.0] ⊗ ComplexF64[1.0; 0.0; 0.0] # start in 10 state
     for ct = 1:n-2
-        ψ0 = ψ0 ⊗ ComplexF64[1.0; 0.0; 0.0]
+        ψ₀ = ψ₀ ⊗ ComplexF64[1.0; 0.0; 0.0]
     end
 
     prop_suite["$n transmons"] = @benchmarkable unitary_propagator($cqs, $times)
-    state_suite["$n transmons"] = @benchmarkable unitary_state($cqs, $times, $ψ0)
+    state_suite["$n transmons"] = @benchmarkable unitary_state($cqs, $times, $ψ₀)
 end
 
 
@@ -101,11 +102,11 @@ state_suite = suite["unitary"]["pure state"]["rotating frame parametric 2Q gate"
 
 for n = 2:4
 
-    q0 = FixedDuffingTransmon("q0", 3.94015, -0.1807,  3)
-    q1 = TunableDuffingTransmon("q1",  0.172, 16.4, 0.55, 3)
+    q0 = DuffingTransmon("q0", 3, DuffingSpec(3.94015, -0.1807))
+    q1 = PerturbativeTransmon("q1",  3, TransmonSpec(0.172, 16.4, 0.55))
 
     # add fixed frequency spectators
-    spectator_qs = [FixedDuffingTransmon("q$ct", 4.0 + 0.1*ct, -(0.2 + 0.01*ct), 3) for ct = 2:(n-1)]
+    spectator_qs = [DuffingTransmon("q$ct", 3, DuffingSpec(4.0 + 0.1*ct, -(0.2 + 0.01*ct))) for ct = 2:(n-1)]
 
     # should get an CZ02 interaction at ≈ 115 MHz
     freq = 115.5/1e3
@@ -121,8 +122,8 @@ for n = 2:4
 
     diff_freq = spec(q0).frequency - q1_freq
     # time dependent flip flop interaction
-    add_hamiltonian!(cqs, rotating_flip_flop(q0, q1, 0.006, diff_freq), [q0,q1])
-    add_hamiltonian!(cqs, flux_drive(q1, t -> amp*sin(2π*freq*t)), q1)
+    add_hamiltonian!(cqs, t -> 0.006*.5*X_Y([q0, q1], [diff_freq*t, 0.0]), [q0,q1])
+    add_hamiltonian!(cqs, parametric_drive(q1, t -> amp*sin(2π*freq*t)), q1)
 
     # add hamiltoians for spectators coupled to tunable transmon
     for q = all_qs[3:end]
@@ -130,15 +131,15 @@ for n = 2:4
         # add rotating frame Hamiltonian shifts
         add_hamiltonian!(cqs, -spec(q).frequency*number(q), q)
         diff_freq = q1_freq - spec(q).frequency
-        add_hamiltonian!(cqs, rotating_flip_flop(q1, q, 0.006, diff_freq), [q1,q])
+        add_hamiltonian!(cqs, t -> 0.006*.5*X_Y([q1, q], [diff_freq*t, 0.0]), [q1, q])
     end
 
-    times = collect(0:200)
-    ψ0 = ComplexF64[0.0; 1.0; 0.0] ⊗ ComplexF64[0.0; 1.0; 0.0] # start in 11 state
+    times = 0:200
+    ψ₀ = ComplexF64[0.0; 1.0; 0.0] ⊗ ComplexF64[0.0; 1.0; 0.0] # start in 11 state
     for ct = 1:n-2
-        ψ0 = ψ0 ⊗ ComplexF64[1.0; 0.0; 0.0]
+        ψ₀ = ψ₀ ⊗ ComplexF64[1.0; 0.0; 0.0]
     end
 
     prop_suite["$n transmons"] = @benchmarkable unitary_propagator($cqs, $times)
-    state_suite["$n transmons"] = @benchmarkable unitary_state($cqs, $times, $ψ0);
+    state_suite["$n transmons"] = @benchmarkable unitary_state($cqs, $times, $ψ₀);
 end
