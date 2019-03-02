3b69392192651f2b063ac24ca9f318dd309e966d
Colm Ryan <colm@rigetti.com>
2019-03-01 17:04:53 -0800

    :green_check: more test coverage for basic ladder operators

--- test/test_operators.jl
+++ test/test_operators.jl
@@ -1,7 +1,26 @@
 using Test, QSimulator
 
-# test harmonic oscillator operator identity a†a = N
-@testset "Resonator" begin
+using LinearAlgebra: I
+@testset "Primitives" begin
     r = Resonator("test", 3, QSimulator.ResonatorSpec(5.5))
-    @test isapprox(raising(r) * lowering(r), number(r))
+
+    # test some trivial cases of the ladder operators
+    @test raising(r) == [0 0 0;1 0 0;0 √2 0]
+    @test lowering(r) == [0 1 0;0 0 √2;0 0 0]
+
+    r = Resonator("test", 7, QSimulator.ResonatorSpec(5.5))
+
+    # test harmonic oscillator operator identity a†a = N
+    @test raising(r) * lowering(r) ≈ number(r)
+
+    # test X = a + a†
+    @test X(r) ≈ raising(r) + lowering(r)
+
+    # test phase rotation of 0.25 on X → Y
+    @test X(r, 0.25) ≈ Y(r)
+
+    # test commutator relation [a, a†] = 1; because of truncation it doesn't hold for last row/columns
+    commutator = lowering(r)*raising(r)- raising(r)*lowering(r)
+    @test commutator[1:end-1,1:end-1] ≈ Matrix{eltype(commutator)}(I, dimension(r)-1, dimension(r)-1)
+
 end
