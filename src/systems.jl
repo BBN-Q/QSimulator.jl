fef8fba67dca288fe405039219042f2bc7b56d8d
Chris Osborn <chris@rigetti.com>
2019-05-24 09:39:18 -0700

    Added docstrings to structs and functions, some minor reorganization.

--- src/systems.jl
+++ src/systems.jl
@@ -8,74 +8,102 @@ export PerturbativeTransmon, ChargeBasisTransmon, DiagonalChargeBasisTransmon, R
 export hamiltonian, fit_transmon
 
 ######################################################
-# QSpec - the physical parameters defining a quantum system
+# Types
 ######################################################
+
+HermitianMatrix = Matrix{<:Number}
 abstract type QSpec end
+abstract type QSystem end
 
-struct TransmonSpec <: QSpec
-   EC::Real
-   EJ1::Real
-   EJ2::Real
-end
+# Required QSystem functions with defaults
+label(q::QSystem)::AbstractString = q.label
+dimension(q::QSystem)::Int64 = q.dimension
+spec(q::QSystem)::QSpec = q.spec
 
-struct DuffingSpec <: QSpec
-   frequency::Real
-   anharmonicity::Real
-end
+################################################################################
+# Specs
+################################################################################
 
-struct ResonatorSpec <: QSpec
-   frequency::Real
-end
+# Structs
 
 struct HermitianSpec <: QSpec
-   matrix::Matrix{<:Number}
+   matrix::HermitianMatrix
    function HermitianSpec(matrix::Matrix{<:Number})
        @assert ishermitian(matrix)
        return new(matrix)
    end
 end
 
-######################################################
-# QSystem - a quantum system
-######################################################
-abstract type QSystem end
+"""
+Resonator, specified by its transition frequency.
+"""
+struct ResonatorSpec <: QSpec
+   frequency::Real
+end
 
-# required functions with defaults
-label(q::QSystem) = q.label
-dimension(q::QSystem) = q.dimension
-spec(q::QSystem) = q.spec
+"""
+Transmon, specified by its charge and junctions energies, EC and EJ1/EJ2,
+respectively.
+"""
+struct TransmonSpec <: QSpec
+   EC::Real
+   EJ1::Real
+   EJ2::Real
+end
 
-######################################################
-# LiteralHermitian, an arbitrary Hamiltonian
-######################################################
+"""
+Alternative Transmon specifiication by its (base linear) transition frequency
+and anharmonicity.
+"""
+struct DuffingSpec <: QSpec
+   frequency::Real
+   anharmonicity::Real
+end
+
+# Functions
+
+"""
+Construct a TransmonSpec (EC, EJ1, EJ2) from a DuffingSpec (frequency,
+anharmonicity), and a model type to perform the conversion.
+
+An optimization routine, fit_transmon, must be run to convert
+frequency/anharmonicity to EC/EJ.
+"""
+function TransmonSpec(t::DuffingSpec, model::Type{T}=PerturbativeTransmon,
+    num_terms::Int=PERTURBATIVE_NUM_TERMS) where {T<:QSystem}
+    return fit_transmon(t.frequency, t.frequency, t.anharmonicity, model, num_terms)
+end
+
+"""
+Construct a DuffingSpec (freqency, anharmonicity) from a TransmonSpec (EC, EJ1,
+EJ2), at a given flux quantum ϕ. May optionally supply a customer number of
+perturbative terms.
+"""
+function DuffingSpec(t::TransmonSpec, ϕ::Real=0.0, num_terms::Int=PERTURBATIVE_NUM_TERMS)
+    freq = perturbative_transmon_freq(t.EC, t.EJ1, t.EJ2, ϕ, num_terms=num_terms)
+    anharm = perturbative_transmon_anharm(t.EC, t.EJ1, t.EJ2, ϕ, num_terms=num_terms)
+    return DuffingSpec(freq, anharm)
+end
+
+################################################################################
+# QSystems.
+# QSystems are Structs comprised of a QSpec, a label and a dimension.
+################################################################################
+
+# Structs
 
 struct LiteralHermitian <: QSystem
     label::AbstractString
     spec::HermitianSpec
 end
 
-dimension(h::LiteralHermitian) = size(h.spec.matrix, 1)
-
-hamiltonian(q::LiteralHermitian) = spec(q).matrix
-
-######################################################
-# Resonator, a linear resonator
-######################################################
+dimension(h::LiteralHermitian)::Int64 = size(h.spec.matrix, 1)
+hamiltonian(q::LiteralHermitian)::HermitianMatrix = spec(q).matrix
 
 struct Resonator <: QSystem
     label::AbstractString
     dimension::Int
     spec::ResonatorSpec
-end
-
-hamiltonian(r::Resonator) = diagm(0 => [spec(r).frequency * n for n in 0:dimension(r)-1])
-
-######################################################
-# DuffingTransmon
-######################################################
-
-function duffing_hamiltonian(freq::Real, anharm::Real, dimension::Int)
-    diagm(0 => [(freq - 0.5 * anharm) * n + 0.5 * anharm * n^2 for n in 0:dimension-1])
 end
 
 struct DuffingTransmon <: QSystem
@@ -84,43 +112,22 @@ struct DuffingTransmon <: QSystem
     spec::DuffingSpec
 end
 
-hamiltonian(t::DuffingTransmon) = duffing_hamiltonian(spec(t).frequency, spec(t).anharmonicity, dimension(t))
-
-hamiltonian(t::DuffingTransmon, frequency::Real) = duffing_hamiltonian(frequency, spec(t).anharmonicity, dimension(t))
-
-######################################################
-# PerturbativeTransmon
-######################################################
-
+"""
+The PerturbativeTransmon type is a DuffingTransmon with the added `num_terms`
+attribute, specifying to how many terms the transmon should be calculated.
+"""
 struct PerturbativeTransmon <: QSystem
     label::AbstractString
     dimension::Int
     spec::TransmonSpec
     num_terms::Int
 
-    PerturbativeTransmon(label::AbstractString, dimension::Int, spec::TransmonSpec; num_terms::Int=PERTURBATIVE_NUM_TERMS) =
-                new(label::AbstractString, dimension::Int, spec::TransmonSpec, num_terms)
+    PerturbativeTransmon(
+        label::AbstractString, dimension::Int, spec::TransmonSpec;
+        num_terms::Int=PERTURBATIVE_NUM_TERMS
+    ) =
+        new(label::AbstractString, dimension::Int, spec::TransmonSpec, num_terms)
 end
-
-function DuffingSpec(t::TransmonSpec, ϕ::Real=0.0, num_terms::Int=PERTURBATIVE_NUM_TERMS)
-    freq = perturbative_transmon_freq(t.EC, t.EJ1, t.EJ2, ϕ, num_terms=num_terms)
-    anharm = perturbative_transmon_anharm(t.EC, t.EJ1, t.EJ2, ϕ, num_terms=num_terms)
-    return DuffingSpec(freq, anharm)
-end
-
-function TransmonSpec(t::DuffingSpec, model::Type{T}=PerturbativeTransmon,
-    num_terms::Int=PERTURBATIVE_NUM_TERMS) where {T<:QSystem}
-    return fit_transmon(t.frequency, t.frequency, t.anharmonicity, model, num_terms)
-end
-
-function hamiltonian(t::PerturbativeTransmon, ϕ::Real=0.0)
-    s = DuffingSpec(spec(t), ϕ, t.num_terms)
-    return duffing_hamiltonian(s.frequency, s.anharmonicity, dimension(t))
-end
-
-######################################################
-# Charge basis transmon
-######################################################
 
 struct ChargeBasisTransmon <: QSystem
     label::AbstractString
@@ -131,22 +138,6 @@ struct ChargeBasisTransmon <: QSystem
         return new(label, dimension, spec)
     end
 end
-
-
-function hamiltonian(t::ChargeBasisTransmon, ϕ::Real=0.0)
-    d = dimension(t)
-    N = floor(Int, d/2)
-    s = spec(t)
-    EJ = sqrt(s.EJ1^2 + s.EJ2^2 + 2 * s.EJ1 * s.EJ2 * cos(2π * ϕ))
-    EC = s.EC
-    charging_term = 4 * EC * diagm(0 => (-N:N).^2)
-    tunneling_term = -0.5 * EJ * (diagm(-1 => ones(d-1), 1 => ones(d-1)))
-    return charging_term + tunneling_term
-end
-
-######################################################
-# Diagonalized charge basis transmon
-######################################################
 
 const CHARGE_NUM_TERMS = 101
 
@@ -162,6 +153,85 @@ struct DiagonalChargeBasisTransmon <: QSystem
     end
 end
 
+struct RotatingFrameSystem <: QSystem
+    label::AbstractString
+    system::QSystem
+    rotation_rate::Real
+end
+
+# contruct from an existing QSystem without a different label
+RotatingFrameSystem(q::QSystem, rotation_rate::Real) = RotatingFrameSystem(label(q), q, rotation_rate)
+
+# defer `dimension` and `spec` to underlying QSystem
+dimension(r::RotatingFrameSystem) = dimension(r.system)
+spec(r::RotatingFrameSystem) = spec(r.system)
+
+################################################################################
+# Hamiltonians
+################################################################################
+
+# Resonator
+
+"""
+Construct a resonator hamiltonian from energy levels equally spaced
+by the resonator frequency, along the diagonal of a square matrix.
+"""
+hamiltonian(r::Resonator)::HermitianMatrix =
+    diagm(0 =>[spec(r).frequency * n for n in 0:dimension(r)-1])
+
+# Duffing Transmon
+
+"""
+Construct the hamiltonian of a duffing transmon from energy levels spaced by the
+transmon's frequency, minus increasing amounts of anharmonicity for increasing
+levels. Satisfies the condition
+
+```
+E_{n+1} - E_n = frequency - anharmonicity.
+```
+"""
+function duffing_hamiltonian(
+    freq::Real, anharm::Real, dimension::Int
+)::HermitianMatrix
+    diagm(0 => [(freq - 0.5 * anharm) * n + 0.5 * anharm * n^2
+                for n in 0:dimension-1])
+end
+
+"""
+Construct a hamiltonian for a Duffing transmon.
+"""
+hamiltonian(t::DuffingTransmon)::HermitianMatrix =
+    duffing_hamiltonian(spec(t).frequency, spec(t).anharmonicity, dimension(t))
+
+"""
+Construct a hamiltonian for a Duffing transmon using a custom frequency.
+"""
+hamiltonian(t::DuffingTransmon, frequency::Real)::HermitianMatrix =
+    duffing_hamiltonian(frequency, spec(t).anharmonicity, dimension(t))
+
+# Perturbative Transmon
+
+"""
+Construct a hamiltonian from a PerturbativeTransmon at a given flux quantum
+ϕ.
+"""
+function hamiltonian(t::PerturbativeTransmon, ϕ::Real=0.0)::HermitianMatrix
+    s = DuffingSpec(spec(t), ϕ, t.num_terms)
+    return duffing_hamiltonian(s.frequency, s.anharmonicity, dimension(t))
+end
+
+# Charge Basis Transmon
+
+function hamiltonian(t::ChargeBasisTransmon, ϕ::Real=0.0)
+    d = dimension(t)
+    N = floor(Int, d/2)
+    s = spec(t)
+    EJ = sqrt(s.EJ1^2 + s.EJ2^2 + 2 * s.EJ1 * s.EJ2 * cos(2π * ϕ))
+    EC = s.EC
+    charging_term = 4 * EC * diagm(0 => (-N:N).^2)
+    tunneling_term = -0.5 * EJ * (diagm(-1 => ones(d-1), 1 => ones(d-1)))
+    return charging_term + tunneling_term
+end
 
 function hamiltonian(t::DiagonalChargeBasisTransmon, ϕ::Real=0.0)
     h = hamiltonian(ChargeBasisTransmon(label(t), t.num_terms, spec(t)), ϕ)
@@ -172,10 +242,38 @@ function hamiltonian(t::DiagonalChargeBasisTransmon, ϕ::Real=0.0)
 end
 # TODO: update raising and lowering to be correct for DiagonalChargeBasisTransmon
 
+# Rotating Frame
 
-######################################################
-# Fit transmons
-######################################################
+function hamiltonian(r::RotatingFrameSystem)
+    h = hamiltonian(r.system)
+    subtract_number!(h, r.rotation_rate)
+    return h
+end
+
+function hamiltonian(r::RotatingFrameSystem, t::Real)
+    h = hamiltonian(r.system, t)
+    subtract_number!(h, r.rotation_rate)
+    return h
+end
+
+# Util
+
+"""
+Helper function that subtracts a real number off the diagonal elements of a Real
+matrix.
+"""
+function subtract_number!(h::Matrix{<:Real}, r::Real)
+    if r != 0
+        for i in 1:size(h, 1)
+            h[i,i] -= (i-1) * r
+        end
+    end
+end
+
+################################################################################
+# Conversion fitting (EC/EJ <> Frequency/Anharmonicity)
+################################################################################
+
 """
     fit_transmon(freq_max::Real, freq_min::Real, anharm_max::Real, model::Type{T}, num_terms::Int) where {T<:QSystem}
 
@@ -193,7 +291,9 @@ If `freq_max == freq_min` then `EJ2 == 0` is enforced.
 ## returns
 A TransmonSpec.
 """
-function fit_transmon(freq_max::Real, freq_min::Real, anharm_max::Real, model::Type{T}, num_terms::Int) where {T<:QSystem}
+function fit_transmon(
+    freq_max::Real, freq_min::Real, anharm_max::Real, model::Type{T}, num_terms::Int
+)::TransmonSpec where {T<:Union{PerturbativeTransmon,DiagonalChargeBasisTransmon}}
     function f_fixed(params)
       EC, EJ = params
       t = model("", 3, TransmonSpec(EC, EJ, 0.0), num_terms=num_terms)
@@ -225,42 +325,5 @@ function fit_transmon(freq_max::Real, freq_min::Real, anharm_max::Real, model::T
     else
         res = optimize(f_tunable, [EC_guess, EJ1_guess, EJ2_guess])
         return TransmonSpec(res.minimizer...)
-    end
-end
-
-######################################################
-# Rotating frame generated by number operator times a constant
-######################################################
-
-struct RotatingFrameSystem <: QSystem
-    label::AbstractString
-    system::QSystem
-    rotation_rate::Real
-end
-
-# contruct from an existing QSystem without a different label
-RotatingFrameSystem(q::QSystem, rotation_rate::Real) = RotatingFrameSystem(label(q), q, rotation_rate)
-
-# defer `dimension` and `spec` to underlying QSystem
-dimension(r::RotatingFrameSystem) = dimension(r.system)
-spec(r::RotatingFrameSystem) = spec(r.system)
-
-function hamiltonian(r::RotatingFrameSystem)
-    h = hamiltonian(r.system)
-    subtract_number!(h, r.rotation_rate)
-    return h
-end
-
-function hamiltonian(r::RotatingFrameSystem, t::Real)
-    h = hamiltonian(r.system, t)
-    subtract_number!(h, r.rotation_rate)
-    return h
-end
-
-function subtract_number!(h::Matrix{<:Real}, r::Real)
-    if r != 0
-        for i in 1:size(h, 1)
-            h[i,i] -= (i-1) * r
-        end
     end
 end
