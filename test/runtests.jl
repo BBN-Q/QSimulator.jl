df9617bc0f987eb3099f72d1428c0cdbdfce9911
Matthew Ware <matt.ware@raytheon.com>
2019-07-04 17:18:56 -0400

    Updated tests.  -MW

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,7 @@ using LinearAlgebra
 include("unitary-tests.jl")
 include("dissipation-tests.jl")
 
-#include("speed_test.jl")
+include("speed_test.jl")
 #speed_test(5)
+
+#include("test_dissipators.jl")
